b2a77a86aeec02c7fbadec01d80fde509ef99bf1
zhongqin0820 <zhongqin0820@163.com>
2017-12-02 17:50:51 +0800

    fix metric display backend issues

--- routes/moni/metric.js
+++ routes/moni/metric.js
@@ -11,27 +11,31 @@ router.get('/', function(req, res) {
     res.redirect('/login');
   }
   else{
-    db.moni_categorys.find({}, function (err, docs) {
-      var loadedURL={};
-      loadedURL.metric=[];
-      loadedURL.normalMenu=[];
-      loadedURL.metric.push.apply(loadedURL.metric,docs[0].metric);
-      for(var i in docs){
-        var obj={};
-        obj.category_name=docs[i].category_name;
-        obj.category_id=docs[i].category_id;
-        loadedURL.normalMenu.push(obj);
+    db.moni_categorys.findOne({"orgId":req.session.moni.user}, function (err, doc) {
+      if(doc!=null){
+        var loadedURL={};
+        loadedURL.metric=[];
+        loadedURL.normalMenu=[];
+        loadedURL.metric.push.apply(loadedURL.metric,doc.categoryArray[0].metric);
+        for(var i in doc.categoryArray){
+          var obj={};
+          obj.category_name=doc.categoryArray[i].category_name;
+          obj.category_id=doc.categoryArray[i].category_id;
+          loadedURL.normalMenu.push(obj);
+        }
+        res.render('moni/metric', {
+          title: '监控显示',
+          result: loadedURL.metric,
+          normalMenu: loadedURL.normalMenu,
+          alertMenu: null,
+          user: req.session.moni.user,
+          activeMetric:true
+        });
       }
-      res.render('moni/metric', {
-        title: '监控显示',
-        result: loadedURL.metric,
-        normalMenu: loadedURL.normalMenu,
-        alertMenu: null,
-        user: req.session.moni.user,
-        activeMetric:true
-      });
+      return;
     });
   }
+  return;
 });
 
 // /metric/normal/:id
@@ -39,25 +43,28 @@ router.get('/monitor/:id', function(req, res) {
   if(req.session.moni.user!=null){
       var db = req.app.db;
       var categoryId = req.params.id-1;
-      db.moni_categorys.find({}, function (err, docs) {
-        var loadedURL={};
-        loadedURL.metric=[];
-        loadedURL.normalMenu=[];
-        loadedURL.metric.push.apply(loadedURL.metric,docs[categoryId].metric);
-        for(var i in docs){
-          var obj={};
-          obj.category_name=docs[i].category_name;
-          obj.category_id=docs[i].category_id;
-          loadedURL.normalMenu.push(obj);
+      db.moni_categorys.findOne({"orgId":req.session.moni.user}, function (err, docs) {
+        if(docs!=null){
+          var loadedURL={};
+          loadedURL.metric=[];
+          loadedURL.normalMenu=[];
+          loadedURL.metric.push.apply(loadedURL.metric,docs.categoryArray[categoryId].metric);
+          for(var i in docs.categoryArray){
+            var obj={};
+            obj.category_name=docs.categoryArray[i].category_name;
+            obj.category_id=docs.categoryArray[i].category_id;
+            loadedURL.normalMenu.push(obj);
+          }
+          res.render('moni/metric', {
+            title: obj.category_name+'显示',
+            result: loadedURL.metric,
+            normalMenu: loadedURL.normalMenu,
+            alertMenu: null,
+            user: req.session.moni.user,
+            activeMetric:true
+          });
         }
-        res.render('moni/metric', {
-          title: obj.category_name+'显示',
-          result: loadedURL.metric,
-          normalMenu: loadedURL.normalMenu,
-          alertMenu: null,
-          user: req.session.moni.user,
-          activeMetric:true
-        });
+      return;
     });
   }else{
     res.redirect('/login');
